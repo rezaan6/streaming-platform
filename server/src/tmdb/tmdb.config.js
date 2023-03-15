91fb7ce8090027d77ffb5fbf976903e310c18810
rezaan6 <rezaan6@gmail.com>
2023-03-15 22:31:21 +0530

    Initial commit

---
+++ server/src/tmdb/tmdb.config.js
@@ -0,0 +1,9 @@
+const baseUrl = process.env.TMDB.BASE_URL;
+const key = process.env.TMDB_KEY;
+
+const getUrl = (endpoint, params) => {
+    const qs = new URLSearchParams(params);
+    return `${baseUrl}${endpoint}?api_key${key}&${qs}`;
+}
+
+export default { getUrl };
