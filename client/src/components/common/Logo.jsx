6b9ebe3d5283b549aea4a1c59fce74f95eefc98c
rezaan6 <rezaan6@gmail.com>
2023-03-17 10:45:20 +0530

    update Logo.jsx, FavoriteList.jsx, PersonDetail.jsx and ReviewList.jsx

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
 
   return (
     <Typography fontWeight="700" fontSize="1.7rem">
-      Moon<span style={{ color: theme.palette.primary.main }}>Flix</span>
+      Moon<span style={{ color: theme.palette.primary.main }}>Stream</span>
     </Typography>
   );
 };
