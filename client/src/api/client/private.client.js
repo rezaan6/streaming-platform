9656c689798b579e8c0bb9bdfb0710affd20e1f0
rezaan6 <rezaan6@gmail.com>
2023-03-29 19:46:26 +0530

    update private.client.js and public.client.js

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = process.env.BASEURL ?? '';
+const baseURL = process.env.REACT_APP_BASEURL ?? '';
 
 const privateClient = axios.create({
   baseURL,
